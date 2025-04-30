2b3373572d2f6699c9f199f4a12a09366bf42c96
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-04-30 22:34:49 +0330

    Add New Features

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+import './index.css';
 // import App from './App';
+import App from  "./App-v1"
 import StarRating from "./StarRating";
 
-function Test(){
-    const [movieRating, setMovieRating] = useState(0);
 
-    return <div><StarRating color='blue' onSteRating={setMovieRating}/>
-    <p>this movie was rated {movieRating} stars</p>
-    </div>
-    }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Test/>
-    {/*<App />*/}
-      <StarRating messages={["terrible","bad","okay","good","amazing"]} />
-      <StarRating maxRating={5} className="" defaultRating = {3}/>
+    <App />
   </React.StrictMode>
 );
