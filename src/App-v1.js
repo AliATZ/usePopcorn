cf3e55c233722341c35e68eb17591b4f032d9c1d
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-05-03 20:45:31 +0330

    Add Escape Button

--- src/App-v1.js
+++ src/App-v1.js
@@ -224,6 +224,19 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onCloseMovie();
   }
 
+  useEffect(function () {
+    function callBack(e){
+      if (e.code === "Escape"){
+        onCloseMovie();
+      }
+    }
+    document.addEventListener("keydown", callBack);
+
+    return function (){
+      document.removeEventListener("keydown", callBack);
+    }
+  }, [onCloseMovie]);
+
   useEffect(function (){
     async function getMovieDetails() {
       setIsLoading(true);
